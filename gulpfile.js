b22a1b935465250d3afca613ff13707d1a5eceb6
marmatva <74786362+marmatva@users.noreply.github.com>
2021-07-28 21:10:24 -0300

    Minificacion de imagenes
    Minificacion de imagenes con gulp, y corrección de las rutas en documento HTML

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const { series, src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 // Funcion que compila sass
 function css(){
@@ -16,10 +17,17 @@ function minificarcss(){
         .pipe( dest('./build/css'))  
 }
 
+function imagenes(){
+    return src('src/img/**/*')
+        .pipe(imagemin())
+        .pipe( dest('./build/img'))
+}
+
 function watchArchivos(){
     watch( 'src/scss/**/*.scss', css);
 }
 
 exports.css = css;
 exports.minificarcss = minificarcss;
+exports.imagenes = imagenes;
 exports.watchArchivos = watchArchivos;
