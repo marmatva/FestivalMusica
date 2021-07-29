93725bef8b382c6fb0d9a68987d90019a20121d1
marmatva <74786362+marmatva@users.noreply.github.com>
2021-07-29 09:53:20 -0300

    Creacion de WebP con Gulp
    Creacion de imagenes en .webp con Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,49 @@
 const { series, src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const notify = require('gulp-notify');
+const webp = require('gulp-webp');
 
+
+const paths={
+    imagenes: 'src/img/**/*',
+    scss: 'src/scss/**/*.scss'
+}
 // Funcion que compila sass
 function css(){
-    return src('src/scss/app.scss')
+    return src(paths.scss) //No afecta que tenga **/*.scss en lugar de app.scss, xq es el unico sin guin bajo al comienzo, por lo que será el unico que se compile
         .pipe( sass() )
-        .pipe( dest('./build/css'))
+        .pipe( dest('./build/css'));
 }
 
 function minificarcss(){
-    return src('src/scss/app.scss')
+    return src(paths.scss)
         .pipe( sass({
             outputStyle: 'compressed'
         }) )
-        .pipe( dest('./build/css'))  
+        .pipe( dest('./build/css'));  
 }
 
 function imagenes(){
-    return src('src/img/**/*')
+    return src(paths.imagenes)
         .pipe(imagemin())
         .pipe( dest('./build/img'))
+        .pipe( notify({message: 'Imagen Minificada'}));
+}
+
+function versionWebp(){
+    return src(paths.imagenes)
+        .pipe( webp() )
+        .pipe( dest('./build/img') )
+        .pipe( notify({message:'Version webP lista'}));
 }
 
 function watchArchivos(){
-    watch( 'src/scss/**/*.scss', css);
+    watch( paths.scss, css);
 }
 
 exports.css = css;
 exports.minificarcss = minificarcss;
 exports.imagenes = imagenes;
 exports.watchArchivos = watchArchivos;
+exports.default = series( css, imagenes, versionWebp, watchArchivos );
