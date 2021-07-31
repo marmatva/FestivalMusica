9c9fe651f06a59752d1cb3b2be2d1c8e5ae08871
marmatva <74786362+marmatva@users.noreply.github.com>
2021-07-31 17:40:17 -0300

    Crea funcionalidad de Galeria
    Permite abrir y cerrar las fotos de la galeria

--- src/js/galeria.js
+++ src/js/galeria.js
@@ -7,10 +7,52 @@ function crearGaleria(){
     for(let i=1; i<=12; i++){
         const imagen = document.createElement('IMG');
         imagen.src=`build/img/thumb/${i}.webp`;
+        imagen.dataset.imagenId=i;
+
+        //Añadir la funcion de mostrar imagen
+        imagen.onclick = mostrarImagen;
+
 
         const lista= document.createElement('LI');
         lista.appendChild(imagen);
         
         galeria.appendChild(lista);
     }
+}
+
+function mostrarImagen(e){
+    const id= parseInt( e.target.dataset.imagenId );
+
+    const imagen=document.createElement('IMG');
+    imagen.src=`build/img/grande/${id}.webp`;
+
+    const overlay = document.createElement('DIV');
+    overlay.appendChild(imagen);
+    overlay.classList.add('overlay');
+
+    // Cuando se da click cierra la imagen
+    overlay.onclick = function(){
+        overlay.remove();
+        body.classList.remove('fijar-body');
+    }
+
+    //Boton para cerrar imagen
+    const cerrarImagen = document.createElement('p');
+    cerrarImagen.textContent = 'X';
+    cerrarImagen.classList.add('btn-cerrar');
+
+    overlay.appendChild(cerrarImagen);
+
+    //Cuando se preciona se cierra imagen
+    cerrarImagen.onclick= function(){
+        overlay.remove();
+        body.classList.remove('fijar-body');
+    }
+
+    //Mostrar en el HTML
+    const body = document.querySelector('body');
+    body.appendChild(overlay);
+    body.classList.add('fijar-body');
+
+
 }
